0bf656872e30f443bc0a1b7354eebe704e9ab360
Andrea Tomasi <tomasiandrea.at@gmail.com>
2019-06-16 13:43:44 +0200

    Moved storage to lib.

--- keyllib/src/lib.rs
+++ keyllib/src/lib.rs
@@ -1,1 +1,33 @@
+use std::collections::HashMap;
+use std::time::{Duration, SystemTime};
+use std::collections::hash_map::RandomState;
+
 pub mod hardware;
+
+pub struct KeystrokeStorage {
+    storage: HashMap<&'static str, u32, RandomState>
+}
+
+impl KeystrokeStorage {
+    pub fn new() -> KeystrokeStorage {
+        KeystrokeStorage { storage: HashMap::new() }
+    }
+    pub fn insert(&mut self, key: &'static str) {
+        let value = *self.storage.get(&key).unwrap_or(&0);
+        self.storage.insert(key, value + 1);
+    }
+    pub fn complete_to_string(&mut self, d: &Duration) -> String {
+        // after 2100?
+        let time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("Cannot compute time").as_secs() as u32;
+        self.storage.insert("__time__", time);
+
+        self.storage.insert("__duration__", d.as_secs() as u32);
+
+        let result = serde_json::to_string(&self.storage).expect("Cannot format json");
+        self.storage.clear();
+        result
+    }
+}
+
--- keylx/src/main.rs
+++ keylx/src/main.rs
@@ -1,12 +1,10 @@
 #![feature(deadline_api)]
 
 use keyllib::hardware::*;
-use std::collections::HashMap;
-use std::time::SystemTime;
+use keyllib::KeystrokeStorage;
 
 fn main() {
     let (sx, rx) = std::sync::mpsc::channel();
-
 
     let _ = std::thread::spawn(move || {
         let mut keyboard = XKeyboard::new(XDisplay::new());
@@ -17,29 +15,18 @@ fn main() {
         }
     });
 
-    let mut m = HashMap::new();
+    let mut storage = KeystrokeStorage::new();
     let d = std::time::Duration::from_secs(10);
     loop {
         let t = std::time::Instant::now();
         let end = t + d;
         loop {
             match rx.recv_deadline(end) {
-                Ok(name) => {
-                    let value = *m.get(&name).unwrap_or(&0);
-                    m.insert(name, value + 1);
-                }
+                Ok(name) => storage.insert(name),
                 Err(_) => break,
             };
         }
 
-        // after 2100?
-        let time = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .expect("Cannot compute time").as_secs() as u32;
-        m.insert("__time__", time);
-        m.insert("__duration__", d.as_secs() as u32);
-
-        println!("{}", serde_json::to_string(&m).expect("Cannot format json"));
-        m.clear();
+        println!("{}", storage.complete_to_string(&d))
     }
 }
